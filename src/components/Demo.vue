6191e25a81bb3ea34efb162a08460f898f37c567
lisong <1990691193@qq.com>
2021-11-25 02:18:27 +0000

    5

---
+++ src/components/Demo.vue
@@ -0,0 +1,11 @@
+<template>
+  <button @click="$emit('addtab', 123)">Demo</button>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Demo',
+})
+</script>
--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -2,25 +2,33 @@
   <q-page padding>
     article
     <article-item ref='itemRef' />
+    <demo @addtab='tabHandle' ></demo>
   </q-page>
 </template>
 
 <script>
 import ArticleItem from 'src/components/ArticleItem.vue'
+import Demo from 'src/components/Demo.vue'
 import { defineComponent, ref, onMounted } from 'vue'
 
 export default defineComponent({
   name: 'Article',
-  components: { ArticleItem },
+  components: { ArticleItem, Demo },
   setup () {
    const itemRef = ref(null)
+   const tabHandle = (payload) => {
+     console.log(payload)
+
+     return 'hahaha'
+   }
 
   onMounted(() => {
     itemRef.value.addTab(1)
   })
-   
+
     return {
-      itemRef
+      itemRef,
+      tabHandle
     }
   }
 })
